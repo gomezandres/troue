8da6d7ae944d5770da535718a7eb95276fb5d58e
Andrés Emilio Gómez <AnGomez@prismamp.com>
2018-11-28 18:14:33 -0300

    first commit

---
+++ src/test/java/com/troue/TroueApplicationTests.java
@@ -0,0 +1,16 @@
+package com.troue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TroueApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
